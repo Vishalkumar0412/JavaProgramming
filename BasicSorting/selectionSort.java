a1a8e832e6c82a994a2a17f2a509d0e7d094c252
vishal kumar <vishakumar041200@gmail.com>
2024-09-02 13:48:05 +0530

    selecction sort added

---
+++ BasicSorting/selectionSort.java
@@ -0,0 +1,26 @@
+package BasicSorting;
+
+public class selectionSort {
+    public static void sort(int arr[]){
+        //outer loop 
+        int minPos=i; 
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1){
+            if()
+        }   
+        
+    }
+}
+   public static void main(String[] args) {
+    int arr[]={6,5,3,9,8,1};
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+"  ");
+    }
+    sort(arr);
+    System.out.println();
+    for(int i=0;i<arr.length;i++){
+    System.out.print(arr[i]+"  ");
+    }
+    
+   }
+}
